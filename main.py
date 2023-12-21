ff8c1f2f22aef5e371da8213377574c7874175de
Zoe Farmer <zoe@dataleek.io>
2023-12-21 16:15:51 -0700

    Most questions answered

--- main.py
+++ main.py
@@ -14,41 +14,190 @@ Start answering questions from gdoc
 
 # stdlib
 import sys
+import os
 import sqlite3
+import functools
+import datetime as dt
 
 # 3rd party
 from pony import orm
 import click
 import polars as pl
 import numpy as np
+import snowflake
+import snowflake.connector
 
 # local
-from plexlib import ROOT, DB, OUTPUT
+from plexlib import ROOT, DB, OUTPUT, with_cache
 from plexlib.data import build_database, fetch_data_from_db
 from plexlib.schema import Account, Media, Stream
 
 
 @click.command()
-@click.option("--rebuild", "-r", default=False, type=bool, is_flag=True, help="Rebuild database? Default NO")
-def main(rebuild):
-    build_database(rebuild)
+def main():
+    """
+    PLEX WRAPPED!!
 
-    pl.Config.set_tbl_rows(100)
+    Ended up importing all data to Snowflake so this will just primarily focus on
+    ingestion, caching, and then answering all the questions!
+    """
+    # build_database(rebuild)
 
+    df = load_dataset(
+        """
+        SELECT *
+        FROM plex_wrapped.output.plex_wrapped_2023_base
+    """
+    )
+    print(sorted(df.columns))
+
+    print(sorted(df.get_column("user").unique()))
+
+    # print(df.filter((pl.col("user") == "zo347") & (pl.col("media_type") != "TV Series")))
+    # return
     # owner_stats()
-    # individual_stats()
+    individual_stats(df)
+    group_stats(df)
 
 
-@orm.db_session
-def individual_stats():
-    for user in Account.select():
-        if "zo" in user.name.lower():
-            query = f"""
-                SELECT
-                FROM stream
-            """
-            df = load_dataset(query)
-            print(user)
+def group_stats(df: pl.DataFrame):
+    """
+    Most popular show
+    Most popular movie
+    Most popular genre
+    Top Actor
+    Who watched the most past 1am
+    Who watched the most during work hours on weekdays (6a - 5p)
+    Percent of users who watch with subtitles
+    """
+    print("Top show")
+    X = (
+        df.filter((pl.col("viewed_at") >= dt.date(2023, 1, 1)) & (pl.col("media_type") == "TV Series"))
+        .group_by("grandparent_title")
+        .agg((pl.col("duration_minutes").sum() / 60).alias("duration_hours"))
+        .sort("duration_hours")
+    )
+    print(X)
+
+    print("Top movie")
+    X = (
+        df.filter((pl.col("viewed_at") >= dt.date(2023, 1, 1)) & (pl.col("media_type") != "TV Series"))
+        .group_by("title")
+        .agg((pl.col("duration_minutes").sum() / 60).alias("duration_hours"))
+        .sort("duration_hours")
+    )
+    print(X)
+
+    print("Top genre")
+    X = stats_by_tag(df, "tags_genre")
+    print(X)
+
+    print("Top star")
+    X = stats_by_tag(df, "tags_star")
+    print(X)
+
+    print("Top director")
+    X = stats_by_tag(df, "tags_director")
+    print(X)
+
+    print("Top country")
+    X = stats_by_tag(df, "tags_country")
+    print(X)
+
+    print("Watched most past 1am MST")
+    X = (
+        df.filter(pl.col("viewed_at") >= dt.date(2023, 1, 1))
+        .with_columns(sleepy_time=pl.col("viewed_at").dt.hour().is_between(1, 6, closed="both"))
+        .filter("sleepy_time")
+        .group_by("user")
+        .agg((pl.col("duration_minutes").sum() / 60).alias("duration_hours"))
+        .sort("duration_hours")
+    )
+    print(X)
+
+    print("Watched most during work hours on weekdays")
+    X = (
+        df.filter(pl.col("viewed_at") >= dt.date(2023, 1, 1))
+        .with_columns(
+            during_work_hours=(
+                pl.col("viewed_at").dt.hour().is_between(8, 17, closed="both")
+                & pl.col("viewed_at").dt.weekday().is_between(1, 5, closed="both")
+            )
+        )
+        .filter("during_work_hours")
+        .group_by("user")
+        .agg((pl.col("duration_minutes").sum() / 60).alias("duration_hours"))
+        .sort("duration_hours")
+    )
+    print(X)
+
+
+def individual_stats(df: pl.DataFrame, user=None):
+    if user is not None:
+        df = df.filter(pl.col("user") == user)
+
+    print("Total watch time")
+    X = (
+        df.filter(pl.col("viewed_at") >= dt.date(2023, 1, 1))
+        .group_by("user")
+        .agg((pl.col("duration_minutes").sum() / 60).alias("duration_hours"))
+        .with_columns(duration_days=pl.col("duration_hours") / 24)
+        .sort("duration_hours")
+    )
+    print(X)
+
+    print("Top show by user")
+    X = (
+        df.filter((pl.col("viewed_at") >= dt.date(2023, 1, 1)) & (pl.col("media_type") == "TV Series"))
+        .group_by("user", "grandparent_title")
+        .agg((pl.col("duration_minutes").sum() / 60).alias("duration_hours"))
+        .sort("user", "duration_hours")
+    )
+    print(X)
+
+    print("Top movie by user")
+    X = (
+        df.filter((pl.col("viewed_at") >= dt.date(2023, 1, 1)) & (pl.col("media_type") != "TV Series"))
+        .group_by("user", "title")
+        .agg((pl.col("duration_minutes").sum() / 60).alias("duration_hours"))
+        .sort("user", "duration_hours")
+    )
+    print(X)
+
+    print("Top genre by user")
+    X = stats_by_tag(df, "tags_genre", by=["user"])
+    print(X)
+
+    print("Top star by user")
+    X = stats_by_tag(df, "tags_star", by=["user"])
+    print(X)
+
+    print("Top director by user")
+    X = stats_by_tag(df, "tags_director", by=["user"])
+    print(X)
+
+    print("Top country by user")
+    X = stats_by_tag(df, "tags_country", by=["user"])
+    print(X)
+
+
+def stats_by_tag(df: pl.DataFrame, column: str, by: list[str] = None) -> pl.DataFrame:
+    if by is None:
+        by = []
+    newcol = column.split("_")[-1]
+    X = (
+        df.filter(
+            (pl.col("viewed_at") >= dt.date(2023, 1, 1))
+            & (pl.col(column).is_not_null())
+            & (pl.col("media_type") != "TV Series")
+        )
+        .with_columns(pl.col(column).str.split("|").alias(newcol))
+        .explode(newcol)
+        .group_by(*by, newcol)
+        .agg((pl.col("duration_minutes").sum() / 60).alias("duration_hours"))
+        .sort(*by, "duration_hours")
+    )
+    return X
 
 
 def owner_stats():
@@ -108,12 +257,19 @@ def owner_stats():
     X.write_csv(OUTPUT / "added_by_genre.csv")
 
 
+@with_cache
 def load_dataset(query):
+    account = os.environ["SNOWFLAKE_ACCOUNT"]
+    user = os.environ["SNOWFLAKE_USER"]
+    passwd = os.environ["SNOWFLAKE_PASSWORD"]
+
+    conn = snowflake.connector.connect(account=account, user=user, password=passwd)
+
     df = pl.read_database(
         query=query,
-        connection=sqlite3.connect("combined.db"),
-        schema_overrides={"rating": float, "audience_rating": float, "year": int},
+        connection=conn,
     )
+
     return df
 
 
--- plexlib/__init__.py
+++ plexlib/__init__.py
@@ -1,5 +1,10 @@
+# stdlib
 import os
 import pathlib
+import functools
+
+# 3rd party
+import polars as pl
 
 ROOT = pathlib.Path()
 DB = ROOT / "combined.db"
@@ -7,3 +12,19 @@ OUTPUT = ROOT / "output"
 
 if not OUTPUT.exists():
     os.mkdir(OUTPUT)
+
+
+def with_cache(func):
+    @functools.wraps(func)
+    def _(*args, **kwargs):
+        cachefile = OUTPUT / f"cache_{func.__name__}.feather"
+        if not cachefile.exists():
+            df: pl.DataFrame = func(*args, **kwargs)
+            df.columns = [c.lower() for c in df.columns]
+            df.write_ipc(cachefile)
+        else:
+            df = pl.read_ipc(cachefile)
+
+        return df
+
+    return _
