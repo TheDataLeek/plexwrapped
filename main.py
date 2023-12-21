23579341cf470c5a5b3149bf01e68f1a9daf350b
Zoe Farmer <zoe@dataleek.io>
2023-12-20 17:18:38 -0700

    Initial aggregations

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ import sqlite3
 from pony import orm
 import click
 import polars as pl
+import numpy as np
 
 # local
-from plexlib import ROOT, DB
+from plexlib import ROOT, DB, OUTPUT
 from plexlib.data import build_database, fetch_data_from_db
 from plexlib.schema import Account, Media, Stream
 
@@ -35,21 +36,57 @@ def main(rebuild):
     # load all media added from last year
     last_year_media_query = "SELECT * FROM media WHERE strftime('%Y', added_date) = '2023'"
     df = load_dataset(last_year_media_query)
-    # Durations
+
+    # Media Added
     X = (
         df.with_columns(
             [
-                pl.when(pl.col("parent_title").is_null())
-                .then(pl.col("title"))
-                .otherwise(pl.col("parent_title"))
-                .alias("name")
+                (
+                    pl.when(pl.col("parent_title").is_null() | (pl.col("parent_title") == ""))
+                    .then(pl.col("title"))
+                    .otherwise(pl.col("parent_title"))
+                    .alias("name")
+                ),
             ]
         )
         .group_by(["sourcedb", "media_type"])
-        .agg(pl.col("name").count())
+        .agg(
+            pl.col("name").count().alias("# Added"),
+            pl.col("duration_minutes").sum(),
+            pl.col("rating").drop_nans().drop_nulls().mean(),
+            pl.col("audience_rating").drop_nans().drop_nulls().mean(),
+        )
+        .with_columns((pl.col("duration_minutes") / 60).alias("hours"))
         .sort(["sourcedb", "media_type"])
     )
     print(X)
+
+    # media added by genre
+    X = (
+        df.with_columns(
+            [
+                (
+                    pl.when(pl.col("parent_title").is_null() | (pl.col("parent_title") == ""))
+                    .then(pl.col("title"))
+                    .otherwise(pl.col("parent_title"))
+                    .alias("name")
+                ),
+                (pl.col("tags_genre").replace("", "UNK").str.split("|").alias("genres")),
+            ]
+        )
+        .with_columns((pl.col("genres").list.first().alias("genre")))
+        .group_by(["sourcedb", "media_type", "genre"])
+        .agg(
+            pl.col("name").count().alias("# Added"),
+            pl.col("duration_minutes").sum(),
+            pl.col("rating").drop_nans().drop_nulls().mean(),
+            pl.col("audience_rating").drop_nans().drop_nulls().mean(),
+        )
+        .with_columns((pl.col("duration_minutes") / 60).alias("hours"))
+        .sort(["sourcedb", "media_type"])
+    )
+    print(X)
+    X.write_csv(OUTPUT / "added_by_genre.csv")
 
 
 def load_dataset(query):
--- plexlib/__init__.py
+++ plexlib/__init__.py
@@ -1,4 +1,9 @@
+import os
 import pathlib
 
 ROOT = pathlib.Path()
 DB = ROOT / "combined.db"
+OUTPUT = ROOT / "output"
+
+if not OUTPUT.exists():
+    os.mkdir(OUTPUT)
